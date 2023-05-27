5fa654c9bc71a393e280332d03c2ecb9d0fa1ac3
Ashot0 <illia.holovan@gmail.com>
2023-05-27 16:16:35 +0300

    all

--- js/script.js
+++ js/script.js
@@ -2,3 +2,25 @@
 "use strict";
 
 
+function gayFunction() {
+	alert('Я тоже');
+};
+
+function notGayFunction() {
+	alert('Нихуя ты изворотливый')
+};
+
+const random = (min, max) => {
+	const rand = min + Math.random() * (max - min + 1);
+	console.log(rand)
+	return Math.floor(rand);
+}
+
+const btnRun = document.getElementById('btn2')
+console.log(btnRun)
+
+btnRun.addEventListener('mouseenter', () => {
+	btnRun.style.left = `${random(0, 90)}%`;
+	btnRun.style.top = `${random(0, 90)}%`;
+	console.log('event Listener')
+})
