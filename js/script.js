eeec73ca03d82685624aeca6492c573a546eda5b
Ashot0 <illia.holovan@gmail.com>
2023-07-02 02:53:19 +0300

    PhoneFix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
 "use strict";
 
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	IOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.IOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	},
+};
+
+
+
 function gayFunction() {
 	alert('Я тоже');
 };
 
 function notGayFunction() {
-	alert('Нихуя ты изворотливый')
+	if (isMobile.any()) {
+	} else {
+		alert('Нихуя ты изворотливый')
+	}
 };
 
 const random = (min, max) => {
